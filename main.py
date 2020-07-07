8bc05c884e81f7b769ccec31e63786909c29f95e
Syarifuddin1991 <67943735+Syarifuddin1991@users.noreply.github.com>
2020-07-07 13:15:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from twitter_scraper_helmi import get_tweets
 
 data = []
 error = 0
-for t in get_tweets('gojekindonesia', pages=1):
+for t in get_tweets('NUgarislucu', pages=10):
     data.append(t)
     if (t['status'] != 'ok'):
         error += 1
