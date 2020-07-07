8c1548b923ecca702ab7ad3bd439956e08d77d07
Syarifuddin1991 <67943735+Syarifuddin1991@users.noreply.github.com>
2020-07-07 13:55:25 +0700

    Update main.py
    from twitter_scraper_helmi import get_tweets
    
    data = []
    error = 0
    for t in get_tweets('NUgarislucu', pages=10):
        data.append(t)
        if (t['status'] != 'ok'):
            error += 1
        
    print('tweet found: ', len(data))
    print('error: ', error)
    
    for i, t in enumerate(data):
        print(t['tweet']['text'], '\n')

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from twitter_scraper_helmi import get_tweets
 
 data = []
 error = 0
-for t in get_tweets('gojekindonesia', pages=1):
+for t in get_tweets('NUgarislucu', pages=10):
     data.append(t)
     if (t['status'] != 'ok'):
         error += 1
@@ -10,5 +10,5 @@ for t in get_tweets('gojekindonesia', pages=1):
 print('tweet found: ', len(data))
 print('error: ', error)
 
-for i, t in enumerate(data):
+for i, t in enumerate(numerate):
     print(t['tweet']['text'], '\n')
